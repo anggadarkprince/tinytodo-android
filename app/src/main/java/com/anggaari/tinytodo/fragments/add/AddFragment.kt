c04bd659498af98512d64f6e576e33645587744c
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-05-29 17:01:46 +0700

    save todo and change color priority

--- app/src/main/java/com/anggaari/tinytodo/fragments/add/AddFragment.kt
+++ app/src/main/java/com/anggaari/tinytodo/fragments/add/AddFragment.kt
@@ -2,23 +2,60 @@ package com.anggaari.tinytodo.fragments.add
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.anggaari.tinytodo.R
+import com.anggaari.tinytodo.data.models.TodoData
+import com.anggaari.tinytodo.data.viewmodel.TodoViewModel
+import com.anggaari.tinytodo.databinding.FragmentAddBinding
+import com.anggaari.tinytodo.fragments.SharedViewModel
 
 class AddFragment : Fragment() {
+    private lateinit var binding: FragmentAddBinding
+    private val todoViewModel: TodoViewModel by viewModels()
+    private val sharedViewModel: SharedViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        // Inflate the layout for this fragment
+        //val rootView = inflater.inflate(R.layout.fragment_add, container, false)
+
+        binding = FragmentAddBinding.inflate(inflater, container, false)
 
         setHasOptionsMenu(true)
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        binding.spinnerPriorities.onItemSelectedListener = sharedViewModel.listener
+
+        return binding.root;
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_add) {
+            insertDataToDB()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertDataToDB() {
+        val title = binding.editTextTitle.text.toString()
+        val priority = binding.spinnerPriorities.selectedItem.toString()
+        val description = binding.editTextDescription.text.toString()
+
+        val isValid = sharedViewModel.verifyDataFromUser(title, description)
+        if (isValid) {
+            val newTodo = TodoData(0, title, sharedViewModel.parsePriority(priority), description)
+            todoViewModel.insertData(newTodo)
+
+            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }
     }
 }
