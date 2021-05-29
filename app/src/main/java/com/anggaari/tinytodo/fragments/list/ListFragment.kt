c45147593e1b01d39fee9af49f1e78199ed7a129
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-05-29 12:29:53 +0700

    add menu and animation

--- app/src/main/java/com/anggaari/tinytodo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/anggaari/tinytodo/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.anggaari.tinytodo.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.anggaari.tinytodo.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -26,6 +24,13 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        // Set top right menu
+        setHasOptionsMenu(true)
+
         return rootView;
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }
